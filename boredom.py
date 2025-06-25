eeb2a9884a5df15b2fb7b1656532ec9f86bc5ddd
taheld77 <taheld77@gmail.com>
2025-06-25 18:01:59 +0300

    done!

--- boredom.py
+++ boredom.py
@@ -1,40 +1,33 @@
 import os
-from os.path import join, getsize
 
-def read_first_file():
+def read_first_file(path):
     """
     this function print the first file from the directory path.
-    :return:
     """
-    while True:
-        path = input("Enter the dir path: ")
-        try:
-            print_or_not = 0
-            index_file_on_dir = 0
-            while print_or_not != 1:
-                new_path = os.path.join(path, os.listdir(path)[index_file_on_dir])
-                try:
-                    with open(new_path,"r") as file:
-                        read_file = file.read()
-                        print(read_file)
-                        print_or_not = 1
-                except PermissionError:
-                    index_file_on_dir += 1
-                except UnicodeDecodeError:
-                    index_file_on_dir += 1
-        except FileNotFoundError:
-            print("dir not found.")
-        except IndexError:
-            print("File not found.")
+    print_or_not = 0
+    index_file_on_dir = 0
+    try:
+
+        while print_or_not != 1:
+            new_path = os.path.join(path, os.listdir(path)[index_file_on_dir])
+            try:
+                with open(new_path,"r") as file:
+                    read_file = file.read()
+                    print(read_file)
+                    print_or_not = 1
+            except PermissionError:
+                index_file_on_dir += 1
+            except UnicodeDecodeError:
+                index_file_on_dir += 1
+    except FileNotFoundError:
+        print("dir not found.")
+    except IndexError:
+        print("File not found.")
 
 def main():
-    read_first_file()
-    path = "C:\\Networks\\test"
-    new_path = os.path.join(path,os.listdir(path)[0])
-    print(new_path)
-    with open(new_path, 'r') as file:
-        read_file = file.read()
-        print(read_file)
+    while True:
+        path = input("Enter the dir path: ")
+        read_first_file(path)
 
 if __name__ == "__main__":
     main()
