5d845ff09a66b6a202c0cc23e3fd2b9f4a3201e8
taheld77 <taheld77@gmail.com>
2025-06-25 19:06:57 +0300

    done!

--- boredom.py
+++ boredom.py
@@ -7,27 +7,26 @@ def read_first_file(path):
     print_or_not = 0
     index_file_on_dir = 0
     try:
-
         while print_or_not != 1:
             new_path = os.path.join(path, os.listdir(path)[index_file_on_dir])
             try:
                 with open(new_path,"r") as file:
                     read_file = file.read()
-                    print(read_file)
                     print_or_not = 1
+                    return read_file
             except PermissionError:
                 index_file_on_dir += 1
             except UnicodeDecodeError:
                 index_file_on_dir += 1
     except FileNotFoundError:
-        print("dir not found.")
+        return "dir not found."
     except IndexError:
-        print("File not found.")
+        return "File not found."
 
 def main():
     while True:
         path = input("Enter the dir path: ")
-        read_first_file(path)
+        print(read_first_file(path))
 
 if __name__ == "__main__":
     main()
